f7c58ead99e15250249aeb61ffece372e3a258c0
hvidales <hectorvee@outlook.com>
2020-05-08 20:55:03 -0700

    Time Delay
    Displays "Hello World" every second for 10 seconds.

---
+++ T02_WaitForTb.vhd
@@ -0,0 +1,15 @@
+entity T02_WaitForTb is
+end entity;
+
+architecture sim of T02_WaitForTb is
+begin
+
+    process is
+    begin
+    
+        report "Hello World!";
+        wait for 1 sec;
+    
+    end process;
+
+end architecture;
